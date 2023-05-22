cab0bfb4e647d427c724c8a7773a033f43511f5c
00402720 <00402720@uca.edu.sv>
2023-05-22 14:22:21 -0600

    Creation of Model
    Model for theme parks and additional dummy data added

---
+++ app/src/main/java/com/example/themeparktracker/data/DummyData.kt
@@ -0,0 +1,11 @@
+package com.example.themeparktracker.data
+
+//List of theme parks around the world :)
+//Source: https://www.skyscanner.es/noticias/la-vuelta-al-mundo-en-10-parques-tematicos
+
+var themeParks = mutableListOf(
+    ThemeParkModel("Disney", "Orlando, Florida"),
+    ThemeParkModel("Port Aventura, Tarragona", "España"),
+    ThemeParkModel("Legoland", "Dinamarca"),
+    ThemeParkModel("Xcaret", "México")
+)
---
+++ app/src/main/java/com/example/themeparktracker/data/ThemeParkModel.kt
@@ -0,0 +1,6 @@
+package com.example.themeparktracker.data
+
+data class ThemeParkModel(
+    val name : String,
+    val location : String
+)
