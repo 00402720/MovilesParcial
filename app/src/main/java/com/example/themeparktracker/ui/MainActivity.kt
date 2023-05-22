3f8ef3adb61b846488e9d2484184c4f17da89d2d
00402720 <00402720@uca.edu.sv>
2023-05-22 14:02:15 -0600

    Creation of ViewModel Structure
    ViewModels created for the fragments to be used

--- app/src/main/java/com/example/themeparktracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/themeparktracker/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.themeparktracker
+package com.example.themeparktracker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.themeparktracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
